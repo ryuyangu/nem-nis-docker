20def69eb06a20b8151a1de2bb8901cca3fa6a55
ryuyangu <ryuyangu@gmail.com>
2018-09-28 15:17:07 +0800

    jvm memory config

--- nis-ncc/nisStart.sh
+++ nis-ncc/nisStart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -Xms${NEM_JAVA_MEMORY}M -Xmx${NEM_JAVA_MEMORY}M -cp ".:package/nis:package/nis/*:package/libs/*" org.nem.deploy.CommonStarter
+java -Xms${NEM_JAVA_MEMORY} -Xmx${NEM_JAVA_MEMORY} -cp ".:package/nis:package/nis/*:package/libs/*" org.nem.deploy.CommonStarter
 #cd nis
 #java -Xms512M -Xmx1G -cp ".:./*:../libs/*" org.nem.deploy.CommonStarter
 #cd -
